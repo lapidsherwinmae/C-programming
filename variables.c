860189d53af992b361d414676b4cd29f835fea18
Dino-16 <ceejaycutie11@gmail.com>
2025-09-05 00:13:07 +0800

    var

---
+++ variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a = 3; // declaring out the output using variables
+    printf("%d", a); // when calling out the int variables, u need to apply the specifier based on data type and put double qoute and always put comma before calling the variable
+
+    return 0;
+}
