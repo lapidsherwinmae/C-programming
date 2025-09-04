1f52b40cb0821cc6ed96a7a1f67fa1050d72b120
Dino-16 <ceejaycutie11@gmail.com>
2025-09-04 23:46:47 +0800

    first-code

---
+++ syntax.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // standard input output library
+
+int main() // main function that runs the code inside it
+{
+    printf("HELLO WORLD!"); // is the one who creates output
+    
+    return 0; // to avoid multiple outputs
+} 
